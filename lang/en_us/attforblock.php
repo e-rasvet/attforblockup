d1a18bdb8c718513a4b55f5b95cfcd231a84394a
Igor Nikulin <dyhanie.istiny@gmail.com>
2016-02-14 17:18:30 +0500

    Master
    First Commit

---
+++ lang/en_us/attforblock.php
@@ -0,0 +1,12 @@
+<?PHP // $Id: attforblock.php,v 1.1.2.2 2009/02/23 19:22:46 dlnsk Exp $ 
+      // block_attendance.php - created with Moodle 1.5.3+ (2005060230)
+
+
+$string['strftimedm'] = '%m.%d';
+$string['strftimedmy'] = '%m.%d.%Y';
+$string['strftimedmyhm'] = '%m.%d.%Y %H.%M'; 
+$string['strftimedmyw'] = '%m.%d.%y&nbsp;(%a)';
+$string['strftimehm'] = '%H:%M'; 
+$string['strftimeshortdate'] = '%m.%d.%Y';
+
+?>
